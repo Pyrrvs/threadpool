e2088029d1fc993c01de2d04df54dc072cc704ef
Gauthier Lamaison <gauthier.lamaison@gmail.com>
2013-07-04 07:46:50 +0200

    Adding license, CMake compatibility

--- src/Worker.cpp
+++ src/Worker.cpp
@@ -1,5 +1,24 @@
+/*
+** Copyright (c) 2013 Gauthier Lamaison.
+** All rights reserved.
+**
+** Redistribution and use in source and binary forms are permitted
+** provided that the above copyright notice and this paragraph are
+** duplicated in all such forms and that any documentation,
+**  advertising materials, and other materials related to such
+** distribution and use acknowledge that the software was developed
+**  by the <organization>.  The name of the
+**  <organization> may not be used to endorse or promote products derived
+** from this software without specific prior written permission.
+** THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+**  IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
 #include <iostream>
 #include "Worker.hpp"
+
+static int g_task_count = 0;
 
 namespace		Thread
 {
@@ -24,9 +43,9 @@ namespace		Thread
     while (1)
       {
 	this->taskLock.lock();
+	std::cout << "Total executed " << g_task_count << std::endl;
 	if (this->tasks.empty())
 	  {
-	    std::cout << "W[" << this << "] No more task" << std::endl;
 	    this->noMoreTaskCond.signal();	
 	    this->taskCond.wait();
 	  }
@@ -35,10 +54,12 @@ namespace		Thread
 	    task = this->tasks.front();
 	    this->status = ACTIVE;
 	    this->tasks.pop();
-	    std::cout << "W[" << this << "] Getting task" << std::endl;
 	    this->taskLock.unlock();
 	    task->process();
 	    this->status = FREE;
+	    this->taskLock.lock();
+	    ++g_task_count;
+	    this->taskLock.unlock();
 	  }	    
 	else
 	  this->taskLock.unlock();
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+/*
+** Copyright (c) 2013 Gauthier Lamaison.
+** All rights reserved.
+**
+** Redistribution and use in source and binary forms are permitted
+** provided that the above copyright notice and this paragraph are
+** duplicated in all such forms and that any documentation,
+**  advertising materials, and other materials related to such
+** distribution and use acknowledge that the software was developed
+**  by the <organization>.  The name of the
+**  <organization> may not be used to endorse or promote products derived
+** from this software without specific prior written permission.
+** THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+**  IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
 #include <iostream>
 #include <unistd.h>
 
@@ -12,14 +29,17 @@ public:
 
   virtual void process(void)
   {
-    this->lock.lock();
-    std::cout << "Hell yeah" << std::endl;
-    this->lock.unlock();
+    // this->lock.lock();
+    // std::cout << "Processing..." << std::endl;
+    // this->lock.unlock();
 # ifdef _WIN32
   Sleep(2000);
 # else
-  sleep(2000);
+  usleep(random() % 1000);
 # endif
+    // this->lock.lock();
+    // std::cout << "Done processing" << std::endl;
+    // this->lock.unlock();
   }
 
 private:
@@ -28,20 +48,24 @@ private:
 
 void test(void)
 {
-  Thread::ThreadPool threadPool(4);
+  Thread::ThreadPool threadPool(10);
   Thread::Lock lock;
 
-  for (int i = 0; i < 12; ++i)
-    threadPool.addTask(new Toto(lock));
-# ifdef _WIN32
-  Sleep(6000);
-# else
-  sleep(6000);
-# endif
+  for (int i, j = 0; j < 10; ++j) {
+    for (i = 0; i < 50; ++i)
+      threadPool.addTask(new Toto(lock));
+    lock.lock();
+    std::cout << "[" << threadPool.getActiveWorkerCount() << "] / ["
+	      << threadPool.getWorkerCount() << "]" << std::endl;
+    lock.unlock();
+  }
+  threadPool.waitForFinish();
 }
+#include <time.h>
 
 int main(void)
 {
+  srandom(time(NULL));
   test();
   return (0);
 }
